1cde175069f385bcf21467e2d47004cd3871ff42
janneh2000 <cjanneh@gmail.com>
2025-01-14 23:03:54 -0300

    Add AboutMe section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import AboutMe from './components/AboutMe';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <section id="home">
-        <h1>Welcome to My Portfolio</h1>
-        <p>This is where I showcase my projects and skills!</p>
-      </section>
+      <AboutMe />
     </div>
   );
 }
