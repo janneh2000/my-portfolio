c7baa00abe4b59ae3729b1890da07c374bfbea68
janneh2000 <cjanneh@gmail.com>
2025-01-15 11:44:40 -0300

    Save changes before rebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import AboutMe from './components/AboutMe';
+import Projects from './components/Projects';
 
 function App() {
   return (
