50929f912bb2f2c056369ac1f4d1005f7e321cc9
janneh2000 <cjanneh@gmail.com>
2025-01-19 10:47:19 -0300

    Add analytics on website

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
     <div className="pt-16">
@@ -12,6 +13,7 @@ function App() {
       <Projects />
       <Contact />
       <Footer />
+      <Analytics />
     </div>
   );
 }
