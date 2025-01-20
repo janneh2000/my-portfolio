7f07da0cb97c29fde4e9141f2f81a83efc17ad22
janneh2000 <cjanneh@gmail.com>
2025-01-20 12:15:42 -0300

    Fix missing @vercel/analytics package

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import { Analytics } from "@vercel/analytics/react"
+import Analytics from "@vercel/analytics/react"
 function App() {
   return (
     <div className="pt-16">
